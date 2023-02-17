c5e75a39754c0eb8af6622d63522a109f9013f7a
Akireddy Jaswanth <19pa1a0501@vishnu.edu.in>
2023-02-17 12:14:14 +0530

    made the suggested changes

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,36 @@
+// //question =>/**
+// *
+// * Assume we are fetching the data from a rest endpoint in the get data function.
+// * we can simulate the same by replacing the setTimeout with fetch api and executing the same in a browser.
+// *
+// */
+
+// function getData(uId) {
+// setTimeout(() => {
+// console.log("Fetched the data!");
+// return "skc@gmail.com";
+// }, 4000);
+// }
+
+// console.log("start");
+// var email = getData("skc");
+// console.log("Email id of the user id is: " + email);
+// console.log("end");
+
+// // How do you solve this problem. How can we wait for till the function execution is completed, so that we can have correct email at line 10?
+
+
+
+
+
+
+
+
+
 async function getData(uId) {
     try {
       const response = await fetch(`exampleapi`);
+      //here the await will wait until all the data is fetched before moving to next statement
       const email = await response.text();
       console.log("Fetched the data!");
       return email;
@@ -8,12 +38,11 @@ async function getData(uId) {
       throw error;
     }
   }
-  
   console.log("start");
   
   (async function () {
     try {
-      const email = await getData("uid");
+      const email = await getData("skc");
       console.log("Email id of the user id is: " + email);
       console.log("end");
     } catch (error) {
@@ -21,3 +50,8 @@ async function getData(uId) {
     }
   })();
   
+
+//output => start 
+// fetched data
+// Email id of the user id is : skc@gmail.com
+//end
